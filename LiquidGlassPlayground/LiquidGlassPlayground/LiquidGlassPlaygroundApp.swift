94b6b05ffcb2329af2603a768cd44624d2dd0410
Szuyun <szuyun@3sidedcube.com>
2026-05-31 15:45:37 +0100

    Create the initial main project

---
+++ LiquidGlassPlayground/LiquidGlassPlayground/LiquidGlassPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidGlassPlaygroundApp.swift
+//  LiquidGlassPlayground
+//
+//  Created by Szuyun Liang on 31/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct LiquidGlassPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
